d72fa348ef3f5a87bc9dfbeda9f98cda822931f8
hb9397 <hb9397@naver.com>
2023-12-18 00:33:06 +0900

    🎉: init: DB_Access_Read 서버 애플리케이션 생성

---
+++ src/main/java/com/sesac/db_access_read/DbAccessReadApplication.java
@@ -0,0 +1,13 @@
+package com.sesac.db_access_read;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbAccessReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbAccessReadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sesac/db_access_read/DbAccessReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesac.db_access_read;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbAccessReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
