a2e83cac25a8530a5a254a776310d0b9540eeefe
hb9397 <hb9397@naver.com>
2024-01-09 14:08:08 +0900

    🐛 fix: ReverseProxy 를 위한 BaseURL 변경
    - 1.0.0

--- src/main/java/com/sesac/member_read_server/member/controller/MemberController.java
+++ src/main/java/com/sesac/member_read_server/member/controller/MemberController.java
@@ -17,7 +17,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/members")
+@RequestMapping("/api/read/v1/members")
 public class MemberController {
 	private final MemberService memberService;
 
